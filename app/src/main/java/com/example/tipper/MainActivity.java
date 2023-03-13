d9e6ae8b06bec666fdbafdc7d78f7f859fa28ea0
Fancia666 <s18425@pjwstk.edu.pl>
2023-03-13 10:35:43 +0100

    poprawka w kdozie

--- app/src/main/java/com/example/tipper/MainActivity.java
+++ app/src/main/java/com/example/tipper/MainActivity.java
@@ -1,43 +1,36 @@
 package com.example.tipper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText; // for bill amount input
-import android.widget.TextView; // for displaying text
-
+import android.widget.EditText;
+import android.widget.TextView;
 import java.text.DecimalFormat;
-import java.text.NumberFormat; // for currency formatting
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView bmiLabel;
     private TextView bmiButtonLabel;
-
     private EditText massEditText;
-
     private EditText heightEditText;
-
     private Button bmiButton;
 
-    // wołane przy tworzeniu
+    // wywołane przy tworzeniu
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bmiLabel = (TextView) findViewById(R.id.bmiLabel);
-        bmiButtonLabel = (TextView) findViewById(R.id.bmiCalculatedLabel);
+        //pola w ktore wpisywane są wartości
+        massEditText = (EditText) findViewById(R.id.editTextMass);
+        heightEditText = (EditText) findViewById(R.id.editTextHeight);
 
-        massEditText =
-                (EditText) findViewById(R.id.editTextMass);
-
+        //guzik do wykonania akcji przeliczenia bmi
         bmiButton = (Button) findViewById(R.id.bmiButton);
 
+        // odnalezienie pola, w które wpisywana będzie przeliczona wartość
+        bmiButtonLabel = (TextView) findViewById(R.id.bmiCalculatedLabel);
+
+        //dodanie akcji dla guzika
         bmiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,26 +48,11 @@ public class MainActivity extends AppCompatActivity {
                     DecimalFormat df = new DecimalFormat();
                     df.setMaximumFractionDigits(2);
 
-
-
-                        // now assign the system
-                        // service to InputMethodManager
-                        InputMethodManager manager
-                                = (InputMethodManager)
-                                getSystemService(
-                                        Context.INPUT_METHOD_SERVICE);
-                        manager
-                                .hideSoftInputFromWindow(
-                                        view.getWindowToken(), 0);
-
-
                     //wpisanie wyniku w pole na ekranie
                     bmiButtonLabel.setText("" + df.format(oo));
 
                 }
             }
         });
-
-        heightEditText = (EditText) findViewById(R.id.editTextHeight);
     }
 }
